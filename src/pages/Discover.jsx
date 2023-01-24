67cd0c4ec379b76e93a63ecd20b9442bdea1834c
OmarAdamMostafa <115920338+OmarAdamMostafa@users.noreply.github.com>
2023-01-24 15:05:07 +0200

    Commit #1.2
    - Updated the Discover component by adding redux tools (useDispatch & useSelector).
    - Completed SongCard and PlayPause components.

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,12 @@
+import { useDispatch, useSelector } from 'react-redux'
+
 import { Error, Loader, SongCard } from '../components'
 import { genres } from '../assets/constants'
 import { useGetTopChartsQuery } from '../redux/services/shazamCore'
 
 const Discover = () => {
+    const dispatch = useDispatch()
+    const { activeSong, isPlaying } = useSelector((state)=>state.player)
     const { data, isFetching, error } = useGetTopChartsQuery()
     const genreTitle = "Pop"
 
@@ -40,7 +44,10 @@ const Discover = () => {
                             <SongCard
                                 key={song.key}
                                 song={song}
-                                i={index}
+                                isPlaying={isPlaying}
+                                activeSong={activeSong}
+                                data={data}
+                                index={index}
                             />
                         ))
                     }
