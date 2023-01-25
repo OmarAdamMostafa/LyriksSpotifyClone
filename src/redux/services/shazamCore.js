dd601fb33701619dbdfc1b0e5a74e8b3ced74687
OmarAdamMostafa <115920338+OmarAdamMostafa@users.noreply.github.com>
2023-01-25 16:00:46 +0200

    Commit #2.3
    - Completed ArtistCard and SearchBar components.
    - Completed Search functionality and page.
    - Completed TopCharts and TopArtists pages.
    - Updated shazamCore by adding 2 additional endpoints (getSongsByGenre, getSongsBySearch).
    - Updated Discover page.

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -16,9 +16,11 @@ export const shazamCoreApi = createApi({
         getRelatedSong: builder.query({ query: (songid) => `v1/tracks/related?track_id=${songid}` }),
         getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
         getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
+        getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+        getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
     })
 })
 
 export const {
-    useGetTopChartsQuery, useGetSongDetailsQuery, useGetRelatedSongQuery, useGetArtistDetailsQuery, useGetSongsByCountryQuery
+    useGetTopChartsQuery, useGetSongDetailsQuery, useGetRelatedSongQuery, useGetArtistDetailsQuery, useGetSongsByCountryQuery, useGetSongsByGenreQuery, useGetSongsBySearchQuery
 } = shazamCoreApi 
